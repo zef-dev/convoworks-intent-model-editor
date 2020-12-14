ce9f91a162ab51388eab77f67b73800d89455718
Nikola Milić <milic.n96@gmail.com>
2020-12-14 15:56:11 +0100

    Setup initial react library

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from '@zef-dev/convoworks-intent-model-editor'
+import '@zef-dev/convoworks-intent-model-editor/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
